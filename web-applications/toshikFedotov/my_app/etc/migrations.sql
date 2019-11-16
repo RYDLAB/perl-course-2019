ba34d6c8b8c30b9d9482dd7b59f2bf369a06e7b5
Anton <tosha.fedotov.2000@gmail.com>
2019-11-16 14:07:08 +0500

    migration-1

---
+++ web-applications/toshikFedotov/my_app/etc/migrations.sql
@@ -0,0 +1,33 @@
+-- 1 up
+BEGIN;
+    CREATE TABLE snippets (
+        id            BIGINT      PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+        title         TEXT        NOT NULL,
+        language_id   SMALLINT    REFERENCES languages (id),
+        is_hide       BOOLEAN     NOT NULL,
+        creating_date TIMESTAMPTZ NOT NULL 
+    );
+
+    CREATE TABLE languages (
+        id   SMALLINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+        name TEXT     NOT NULL UNIQUE
+    );
+    
+    CREATE TABLE codefiles (
+        id          BIGINT   PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+        snippet_id  BIGINT   REFERENCES snippets (id) ON DELETE CASCADE,
+        queue_num   SMALLINT NOT NULL,
+        content     TEXT     NOT NULL 
+    );
+
+    CREATE TABLE encrypted_links (
+        encrypted_link TEXT   PRIMARY KEY
+        id             BIGINT REFERENCES snippets(id) ON DELETE CASCADE
+    );
+END;
+-- 1 down
+BEGIN;
+    DROP TABLE snippets;
+    DROP TABLE languages;
+    DROP TABLE codefiles;
+END;
