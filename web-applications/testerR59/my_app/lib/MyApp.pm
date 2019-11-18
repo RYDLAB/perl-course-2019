76143cdf0f5b10f9fb91a514b5ac73dccf98644c
user <user@i5.localdomain>
2019-11-19 00:18:33 +0500

    web-task-1: migrations.sql

---
+++ web-applications/testerR59/my_app/lib/MyApp.pm
@@ -0,0 +1,33 @@
+package MyApp;
+use Mojo::Base 'Mojolicious';
+use Mojo::Pg;
+
+# This method will run once at server start
+sub startup {
+  my $self = shift;
+
+  # Load configuration from hash returned by config file
+  my $config = $self->plugin('Config');
+
+  # Configure the application
+  $self->secrets($config->{secrets});
+
+  # Router
+  my $r = $self->routes;
+
+  # Normal route to controller
+  $r->get('/')->to('example#welcome');
+
+  my $db = Mojo::Pg->new($config->{postgresql}{url});
+  $db->migrations->from_file($self->home->rel_file('./etc/migrations.sql'));
+
+  $self->attr(
+    db => sub { $db },
+  );
+
+  $self->attr(
+    pg => sub { $self->db->db },
+  );
+}
+
+1;
