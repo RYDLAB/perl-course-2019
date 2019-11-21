61ac5d85fe930b5aeaa342784f57b11a2c84a662
leoltron <leoole5@gmail.com>
2019-11-22 02:01:25 +0500

    Fixes

--- web-applications/Leoltron/code_chunks/public/scripts/list.js
+++ web-applications/Leoltron/code_chunks/public/scripts/list.js
@@ -18,51 +18,60 @@ function elementInViewport(el) {
     );
 }
 
+
 function createSnippetNode(snippet) {
     var root = document.createElement('article');
     root.className = "snippet_preview";
 
-    var pre = document.createElement('pre');
-    var code = document.createElement('code');
-    code.appendChild(document.createTextNode(snippet["text_short"]));
-    pre.appendChild(code);
-    root.appendChild(pre);
-    hljs.highlightBlock(pre);
-    hljs.lineNumbersBlock(pre);
-
-    var controls = document.createElement('div');
-    controls.className = "controls";
-
-    var buttons = document.createElement('div');
-    buttons.className = "button_row";
-
-    var showFull = document.createElement('a');
-    showFull.className = "button button_primary";
-    showFull.href = "/" + snippet['url'];
-    showFull.innerHTML = 'Show full';
-    buttons.appendChild(showFull);
-
-    var raw = document.createElement('a');
-    raw.className = "button button_secondary";
-    raw.href = "/" + snippet['url'] + '/raw';
-    raw.innerHTML = 'Raw';
-    buttons.appendChild(raw);
-
-    controls.appendChild(buttons);
-
-    var size = document.createElement('div');
-    size.className = "controls__size";
-    size.innerHTML = "" + snippet['total_length'] + " characters";
-    controls.appendChild(size);
-
-    var date = document.createElement('div');
-    date.className = "controls__creationDate";
-    date.innerHTML = "" + snippet['creation_date'] + " characters";
-    controls.appendChild(date);
-
-    root.appendChild(controls);
+    addControls();
+    addCode();
 
     return root;
+
+    function addControls() {
+        var controls = document.createElement('div');
+        controls.className = "controls";
+
+        var buttons = document.createElement('div');
+        buttons.className = "button_row";
+
+        var showFull = document.createElement('a');
+        showFull.className = "button button_primary";
+        showFull.href = "/" + snippet['url'];
+        showFull.innerHTML = 'Show full';
+        buttons.appendChild(showFull);
+
+        var raw = document.createElement('a');
+        raw.className = "button button_secondary";
+        raw.href = "/" + snippet['url'] + '/raw';
+        raw.innerHTML = 'Raw';
+        buttons.appendChild(raw);
+
+        controls.appendChild(buttons);
+
+        var size = document.createElement('div');
+        size.className = "controls__size";
+        size.innerHTML = "" + snippet['total_length'] + " characters";
+        controls.appendChild(size);
+
+        var date = document.createElement('div');
+        date.className = "controls__creationDate";
+        date.innerHTML = "" + snippet['creation_date'];
+        controls.appendChild(date);
+
+        root.appendChild(controls);
+    }
+
+    function addCode() {
+        var pre = document.createElement('pre');
+        var code = document.createElement('code');
+        code.appendChild(document.createTextNode(snippet["text_short"]));
+        pre.appendChild(code);
+        root.appendChild(pre);
+        hljs.highlightBlock(pre);
+        hljs.lineNumbersBlock(pre);
+    }
+
 }
 
 function loadMorePages() {
@@ -85,12 +94,13 @@ function loadMorePages() {
                 })
                 .then(response => response.json())
                 .then(snippets => {
-                    if(snippets.length === 0){
+                    if (snippets.length === 0) {
                         loadedAll = true;
                     }
                     var snippet_root = document.querySelector('.article_list');
                     snippets.forEach(s => snippet_root.appendChild(createSnippetNode(s)));
-                });
+                })
+                .catch(e => console.error(e));
         }
     }
 }
