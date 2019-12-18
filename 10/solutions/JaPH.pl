39e652368949b636da325dc4ded663ee3f94effa
TheEag1leS0n <egorgalembo@mail.ru>
2019-12-18 17:12:08 +0400

    deleted

--- 10/solutions/JaPH.pl
+++
@@ -1,5 +0,0 @@
-my $str = "MXVW#DQRWKHU#SHUO#KDFNHU";
-for my $i(0..length($str)){
-	print chr(ord(substr($str, $i, 1)) - 3);
-}
-
--- 10/solutions/myapp.pl
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env perl
-use Mojolicious::Lite;
-
-open my $fh, '<', $0;
-my $code = do { local $/; <$fh> };
-get $0 =~ /([^\/]+)$/ => sub { shift->render(text => $code); };
-app->start;
