7ff98efdb90b15d4138ac7f7d4390bd6972d2c90
SvetlanaNesterova <nesterova.svetlanka@yandex.ru>
2019-12-18 17:59:43 +0500

    GOOOOLF

--- 10/solutions/SvetlanaNesterova/quine.pl
+++ 10/solutions/SvetlanaNesterova/quine.pl
@@ -1,10 +1,1 @@
-#!/usr/bin/env perl
-use Mojolicious::Lite;
-use File::Slurp;
-use File::Basename;
-
-my $text = read_file($0);
-get basename($0) => sub {
-    shift->render(text => $text);
-};
-app->start;
+use Mojolicious::Lite;use File::Slurp;use File::Basename;get basename($0)=>sub{shift->render(text=>read_file($0))};app->start;
